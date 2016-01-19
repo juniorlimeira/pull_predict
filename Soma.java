2c3589a13042ba6fad158428f71dd2970768c374
Manoel Limeira de Lima Júnior <mlimeira@ic.uff.br>
2016-01-19 16:05:53 -0500

    Atualização do arquivo Soma.java

---
+++ Soma.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Soma{
+  public static void main (String [] args){
+    int a=0, b=0, soma=0;
+    Scanner ler = new Scanner(System.in);
+    a = ler.nextInt();
+    b = ler.nextInt();
+    soma = a + b;
+    System.out.println("Soma:"+soma);
+  }
+}
